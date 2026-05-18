cc1d99c53ada6a54fc0b25297559bba02f5c9da6
Visnja Maksimovic <visnja.maksimovic.bn@gmail.com>
2026-05-18 15:51:25 +0200

    Improve alarm log formatting

--- GalaxyPPG.Server/Alarms/AlarmFileLogger.cs
+++ GalaxyPPG.Server/Alarms/AlarmFileLogger.cs
@@ -16,21 +16,17 @@ namespace GalaxyPPG.Server.Alarms
 
         public void HandleAlarm(object sender, AlarmEventArgs e)
         {
-            string line = string.Format(
-                CultureInfo.InvariantCulture,
-                "{0:o}|{1}|{2}|{3}|{4}|{5}|{6}",
-                e.CreatedAtUtc,
-                e.Participant.ParticipantCode,
-                e.Participant.DeviceName,
-                e.Code,
-                e.Record.SensorType,
-                e.Record.Value,
-                e.Message);
-
             using (FileStream fileStream = new FileStream(logFilePath, FileMode.Append, FileAccess.Write, FileShare.Read))
             using (StreamWriter writer = new StreamWriter(fileStream, Encoding.UTF8))
             {
-                writer.WriteLine(line);
+                writer.WriteLine("Time: {0:yyyy-MM-dd HH:mm:ss} UTC", e.CreatedAtUtc);
+                writer.WriteLine("Participant: {0}", e.Participant.ParticipantCode);
+                writer.WriteLine("Device: {0}", e.Participant.DeviceName);
+                writer.WriteLine("Alarm: {0}", e.Code);
+                writer.WriteLine("Sensor: {0}", e.Record.SensorType);
+                writer.WriteLine("Value: {0}", e.Record.Value.ToString("0.###", CultureInfo.InvariantCulture));
+                writer.WriteLine("Message: {0}", e.Message);
+                writer.WriteLine(new string('-', 60));
             }
         }
     }
